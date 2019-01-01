0d79b9d3a13cabf8af9bb93bcac2dd30bf38c5e6
Lee jaeyoung <dldustn14@gmail.com>
2019-01-02 04:58:38 +0900

    Revert "add/design_main_presentation"
    This reverts commit d3e034b88097bd90a7f5b5411f87d4a692306c25.

--- app/src/main/java/com/example/jaeyoung/movie/MainActivity.java
+++ app/src/main/java/com/example/jaeyoung/movie/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.jaeyoung.movie;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onButtonClicked(View v) {
-        Toast.makeText(getApplicationContext(), "버튼이 눌렸습니다." , Toast.LENGTH_LONG).show();
-    }
-
-    public void onButton2Clicked(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http:/m.naver.com"));
-        startActivity(intent);
     }
 }
