d3e034b88097bd90a7f5b5411f87d4a692306c25
Lee jaeyoung <dldustn14@gmail.com>
2019-01-02 04:58:04 +0900

    add/design_main_presentation 애플리케이션 아이콘, 화면 디자인 추가 화면 구성 layout 수정이 필요함

--- app/src/main/java/com/example/jaeyoung/movie/MainActivity.java
+++ app/src/main/java/com/example/jaeyoung/movie/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.jaeyoung.movie;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v) {
+        Toast.makeText(getApplicationContext(), "버튼이 눌렸습니다." , Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http:/m.naver.com"));
+        startActivity(intent);
     }
 }
